889157e4f28d6b23e280cfb3dcaee0b7ce969b51
AnEvilPenguin <siu06dl@gmail.com>
2021-02-22 13:12:27 +0000

    Create Install-PowerShellCore.ps1
    Quick script to check for pwsh and install the latest version if not already installed.

---
+++ Install-PowerShellCore.ps1
@@ -0,0 +1,28 @@
+If (-not (Test-Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\pwsh.exe')) {
+    $Response = Invoke-WebRequest -uri 'https://aka.ms/powershell-release?tag=stable' -UseBasicParsing
+
+    If ([System.IntPtr]::Size -eq '8') {
+        $Link = ($Response.Links | Where-Object {$_ -like "*64.msi*"}).href
+    }
+    Else {
+        $Link = ($Response.Links | Where-Object {$_ -like "*86.msi*"}).href
+    }
+
+    $MSI = $($Link.Split('/')[-1])
+    [system.net.Webclient]::new().DownloadFile("https://www.github.com$Link","$env:TEMP\$MSI")
+
+    If (Test-Path -Path "$env:TEMP\$MSI") {
+        $Arguments = @(
+            '/i'
+            "$env:TEMP\$MSI"
+            '/qn'
+            '/norestart'
+        )
+        Start-Process -FilePath "$env:systemroot\system32\msiexec.exe" -ArgumentList $Arguments -Wait
+    }
+    Else {
+        Write-Error -Message "Something went wrong downloading the latest pwsh msi from https://www.github.com$Link" -RecommendedAction 'Install it manually' -Category ObjectNotFound -ErrorAction Stop
+    }
+    #Sanity Check     
+}
+#Checks if pwsh is installed and installs the latest version if not
